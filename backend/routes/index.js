5e542f502c25335a62083dc7c98be0ddd10e07a1
Arian <arianmarkus77@yahoo.com>
2017-10-30 14:36:08 +0700

    make dummy

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const DATA = require("./data.json");
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.send(DATA);
 });
 
 module.exports = router;
