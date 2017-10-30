ba0266ee15cc9e5603dba4dcffe5cef141449e54
Arian <arianmarkus77@yahoo.com>
2017-10-30 15:03:26 +0700

    Create constructor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,13 @@ import React, { Component } from "react";
 const API_URL = "https://backend-lzvaodeteb.now.sh";
 
 class App extends Component {
+constructor(props)
+super(props)
+this.state={
+  data:[]
+}
+}
+
   render() {
     return (
       <div id="container">
