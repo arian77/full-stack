6a2ccc05c199ccb21c5c165ebe422d91a1206e3c
Arian <arianmarkus77@yahoo.com>
2017-10-30 15:35:09 +0700

    Fix frontend and backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from "react";
 
-const API_URL = "https://backend-lzvaodeteb.now.sh";
+const API_URL = "http://localhost:8000";
 
 class App extends Component {
-constructor(props)
-super(props)
-this.state={
-  data:[]
-}
-}
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+
+  async componentWillMount() {
+    const RESPONSE = await fetch(API_URL);
+    const DATA = await RESPONSE.json();
+    console.log(DATA);
+    this.setState({
+      data: DATA
+    });
+  }
 
   render() {
     return (
       <div id="container">
-        <p> {API_URL}</p>
+        <p> {JSON.stringify(this.state.data)}</p>
       </div>
     );
   }
